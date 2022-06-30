a6d37e70e7de0fd5c45f68dd57e31799c2c50faf
Oscar Chan <oscarchan96@outlook.com>
2022-06-30 16:15:57 -0500

    pequeños cambios

---
+++ js/cuenta.js
@@ -0,0 +1,11 @@
+function loadUserData(){
+    let user = getSession();
+    document.getElementById("user-name").innerHTML = user.name;
+    document.getElementById("user-cash").innerHTML = user.cash;
+}
+
+function deposito(){
+    let dinero = document.getElementById("cash").value;
+    let user = getSession();
+    let suma = user.cash + parseInt(dinero);
+}
--- js/login.js
+++ js/login.js
@@ -1,55 +1,13 @@
-let flagBtn;
-const EXPIRATION_TIME = 500000;
-const userAccounts = [
-    {
-        name: "Oscar Chan",
-        user: "oscar",
-        cash: 55000,
-        password: "12345",
-        expiration: "No"
-    },
-    {
-        name: "Alejandro Castillo",
-        user: "alex",
-        cash: 10000,
-        password: "23456",
-        expiration: "No"
-    },
-    {
-        name: "Maria Jose",
-        user: "majo",
-        cash: 100000,
-        password: "34567",
-        expiration: "No"
-    }
-];
-
 function ingresar() {
     let user = document.getElementById("input-user").value;
     let password = document.getElementById("input-password").value;
     console.log(`Usuario: ${user} Password: ${password}`);
-    validUser(user,password);
-}
-
-function validUser(user,password) {
-    let users = Object.keys(userAccounts);
-
-    for (let i = 0; i < users.length; i++) {
-        let userObject = users[i];
-        if(userAccounts[userObject].user == user && userAccounts[userObject].password == password)
-            window.location.replace("cuenta.html");
-        //console.log(userAccounts[userObject]);
-    }
-}
-
-function consultarSaldo(){
-    let users = Object.keys(userAccounts);
-
-    for (let i = 0; i < users.length; i++) {
-        let userObject = users[i];
-        let saldo = document.createElement("p");
-        
-        //console.log(userAccounts[userObject]);
+    let nickname = validUser(user,password);
+    if(nickname){
+        createSession(nickname);
+        window.location.replace("cuenta.html");
+    }else{
+        alert("Usuario inexistente");
     }
 }
 
